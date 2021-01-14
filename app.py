420551563939fa3894d5bc3b700c1600482d9b23
Samuel Haile <samishken@gmail.com>
2021-01-14 13:51:00 -0500

    added comment

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
+    # Function returns string everytime we create a new element
     def __repr__(self):
         return '<Task %r>' % self.id
 
